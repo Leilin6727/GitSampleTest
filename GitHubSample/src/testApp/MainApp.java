910a58dd56d010a5f98c304be0dff334c0566202
Mark Kuo <bubugp@gmail.com>
2021-02-01 16:24:29 +0800

    3rd Version

--- GitHubSample/src/testApp/MainApp.java
+++ GitHubSample/src/testApp/MainApp.java
@@ -5,7 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
-
+		//user2 modify---dogfa
+		System.out.println("3nd Version");
 	}
 
 }
